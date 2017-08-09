dac3963fff095949aef66effb7494b0c3caa063d
maikel <maikel8907@gmail.com>
2017-08-09 13:56:31 -0400

    Adjusting social sites

--- Website/settings.py
+++ Website/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
+
+MEDIA_ROOT = '/media/'
+MEDIA_URL = '/media/'
 
 #TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
 
